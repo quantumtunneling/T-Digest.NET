3fee93642b3f0ef0aecb10e0b97d945f1acc9cc7
Graham <greatgrahambini@gmail.com>
2015-06-13 12:42:45 -0700

    Cleanup Project

--- TDigest/TDigest.cs
+++ TDigest/TDigest.cs
@@ -7,30 +7,6 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 
 namespace TDigest {
-    public static class Ext {
-        private static Random _rand = new Random();
-
-        public static void Shuffle<T>(this System.Collections.Generic.IList<T> list) {
-            int n = list.Count;
-            while (n > 1) {
-                n--;
-                int k = _rand.Next(n + 1);
-                T value = list[k];
-                list[k] = list[n];
-                list[n] = value;
-            }
-        }
-    }
-
-    internal class Centroid {
-        public double Mean { get; set; }
-        public double Count { get; set; }
-
-        public Centroid(double mean, double count) {
-            Mean = mean;
-            Count = count;
-        }
-    }
 
     public class TDigest {
         private C5.TreeDictionary<double, Centroid> _centroids;
@@ -39,16 +15,11 @@ namespace TDigest {
         private double _delta;
         private double _K;
 
-        public static void Main(string[] args) {
-            TDigest t = new TDigest();
-
-            var r = new Random();
-            for (int i = 0; i < 1000000; i++) {
-                t.Update(r.NextDouble() * 100);
-            }
-            var nn = t.Quantile(.999);
-        }
-
+        /// <summary>
+        /// Construct a T-Digest
+        /// </summary>
+        /// <param name="delta">delta value</param>
+        /// <param name="K">K value</param>
         public TDigest(double delta = 0.01, double K = 25) {
             _centroids = new TreeDictionary<double, Centroid>();
             _rand = new Random();
@@ -57,74 +28,19 @@ namespace TDigest {
             _K = K;
         }
 
-        private double ComputeCentroidQuantile(Centroid centroid) {
-            double sum = 0;
-            foreach (var c in _centroids.Values) {
-                if (c.Mean > centroid.Mean) break;
-                sum += c.Count; 
-            }
-
-            double denom = _count;
-            return (centroid.Count / 2 + sum) / denom;
-        }
-
-        private Centroid[] GetNeighbors(double x, bool inclusive) {
-            C5.KeyValuePair<double, Centroid> successor;
-            C5.KeyValuePair<double, Centroid> predecessor;
-            bool hasSuccessor;
-            bool hasPredecessor;
-
-            if (inclusive) {
-                hasSuccessor = _centroids.TryWeakSuccessor(x, out successor);
-                hasPredecessor = _centroids.TryWeakPredecessor(x, out predecessor);
-            }
-            else {
-                hasSuccessor = _centroids.TrySuccessor(x, out successor);
-                hasPredecessor = _centroids.TryPredecessor(x, out predecessor);
-            }
-
-            if (hasSuccessor && hasPredecessor) return new[] { successor.Value, predecessor.Value };
-            else if (hasSuccessor) return new[] { successor.Value };
-            else if (hasPredecessor) return new[] { predecessor.Value };
-            else return null;
-        }
-
-        private IEnumerable<Centroid> GetClosestCentroids(double x) {
-            C5.KeyValuePair<double, Centroid> successor;
-            C5.KeyValuePair<double, Centroid> predecessor;
-            bool hasSuccessor;
-            bool hasPredecessor;
-
-            hasSuccessor = _centroids.TryWeakSuccessor(x, out successor);
-            hasPredecessor = _centroids.TryWeakPredecessor(x, out predecessor);
-
-            if (hasPredecessor & hasPredecessor) {
-                var aDiff = Math.Abs(successor.Value.Mean - x);
-                var bDiff = Math.Abs(predecessor.Value.Mean - x);
-
-                if (aDiff < bDiff) yield return successor.Value;
-                else if (bDiff < aDiff) yield return predecessor.Value;
-                else {
-                    yield return successor.Value;
-                    yield return predecessor.Value;
-                }
-            }
-            else if (hasSuccessor) yield return successor.Value;
-            else yield return predecessor.Value;
-        }
-
-        private double GetThreshold(double q) {
-            return 4 * _count * _delta * q * (1 - q);
-        }
-
-        public void Update(double x, double w = 1) {
+        /// <summary>
+        /// Add a new value to the T-Digest
+        /// </summary>
+        /// <param name="value">The value to add</param>
+        /// <param name="w">The relative weight associated with this value. Default is 1 for all values.</param>
+        public void Add(double value, double w = 1) {
             _count += w;
             if (_centroids.Count == 0) {
-                _centroids.Add(x, new Centroid(x, w));
+                _centroids.Add(value, new Centroid(value, w));
                 return;
             }
 
-            var candidates = GetNeighbors(x, true)
+            var candidates = GetClosestCentroids(value)
                 .Select(c => new {
                     Threshold = GetThreshold(ComputeCentroidQuantile(c)),
                     Centroid = c
@@ -138,27 +54,22 @@ namespace TDigest {
 
                 _centroids.Remove(c.Centroid.Mean);
                 c.Centroid.Count += delta_w;
-                c.Centroid.Mean += delta_w * (x - c.Centroid.Mean) / c.Centroid.Count;
+                c.Centroid.Mean += delta_w * (value - c.Centroid.Mean) / c.Centroid.Count;
                 _centroids.Add(c.Centroid.Mean, c.Centroid);
                 w -= delta_w;
                 
                 candidates.Remove(c);
             }
 
-            if (w > 0) _centroids.Add(x, new Centroid(x, w));
+            if (w > 0) _centroids.Add(value, new Centroid(value, w));
             if (_centroids.Count > (_K / _delta)) Compress();
         }
 
-        public void Compress() {
-            var newTDigest = new TDigest(_delta, _K);
-            var temp = _centroids.Values.ToList();
-            temp.Shuffle();
-            foreach (var centroid in temp) {
-                newTDigest.Update(centroid.Mean, centroid.Count);
-            }
-            _centroids = newTDigest._centroids; 
-        }
-
+        /// <summary>
+        /// Estimates the specified quantile
+        /// </summary>
+        /// <param name="quantile">The quantile to esimtate. Must be between 0 and 1.</param>
+        /// <returns>The value for the estimated quantile</returns>
         public double Quantile(double quantile) {
             if (quantile < 0 || quantile > 1) {
                 throw new ArgumentOutOfRangeException("quantile must be between 0 and 1");
@@ -194,6 +105,76 @@ namespace TDigest {
             }
 
             return last.Mean;
+        }
+
+        private double ComputeCentroidQuantile(Centroid centroid) {
+            double sum = 0;
+            foreach (var c in _centroids.Values) {
+                if (c.Mean > centroid.Mean) break;
+                sum += c.Count; 
+            }
+
+            double denom = _count;
+            return (centroid.Count / 2 + sum) / denom;
+        }
+
+        private IEnumerable<Centroid> GetClosestCentroids(double x) {
+            C5.KeyValuePair<double, Centroid> successor;
+            C5.KeyValuePair<double, Centroid> predecessor;
+            bool hasSuccessor;
+            bool hasPredecessor;
+
+            hasSuccessor = _centroids.TryWeakSuccessor(x, out successor);
+            hasPredecessor = _centroids.TryWeakPredecessor(x, out predecessor);
+
+            if (hasPredecessor & hasPredecessor) {
+                var aDiff = Math.Abs(successor.Value.Mean - x);
+                var bDiff = Math.Abs(predecessor.Value.Mean - x);
+
+                if (aDiff < bDiff) yield return successor.Value;
+                else if (bDiff < aDiff) yield return predecessor.Value;
+                else {
+                    yield return successor.Value;
+                    yield return predecessor.Value;
+                }
+            }
+            else if (hasSuccessor) yield return successor.Value;
+            else yield return predecessor.Value;
+        }
+
+        private double GetThreshold(double q) {
+            return 4 * _count * _delta * q * (1 - q);
+        }
+
+        private void Compress() {
+            var newTDigest = new TDigest(_delta, _K);
+            var temp = _centroids.Values.ToList();
+            Shuffle(temp);
+            foreach (var centroid in temp) {
+                newTDigest.Add(centroid.Mean, centroid.Count);
+            }
+            _centroids = newTDigest._centroids; 
+        }
+
+        private void Shuffle(System.Collections.Generic.IList<Centroid> centroids) {
+            int n = centroids.Count;
+            while (n > 1) {
+                n--;
+                int k = _rand.Next(n + 1);
+                Centroid c = centroids[k];
+                centroids[k] = centroids[n];
+                centroids[n] = c;
+            }
+        }
+    }
+
+    internal class Centroid {
+        public double Mean { get; set; }
+        public double Count { get; set; }
+
+        public Centroid(double mean, double count) {
+            Mean = mean;
+            Count = count;
         }
     }
 }
