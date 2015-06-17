dba277db24656fc7bde1bcbc7761419edd1fe701
Graham <greatgrahambini@gmail.com>
2015-06-16 21:14:23 -0700

    Fix bug

--- TDigest/Extensions.cs
+++ TDigest/Extensions.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TDigest {
+namespace StatsLib {
     public static class Extensions {
 
         public static void Shuffle<T>(this IList<T> list) {
