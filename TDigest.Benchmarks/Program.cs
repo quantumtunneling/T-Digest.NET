581e3e9e6a8253491873da6997c4d16134abdafa
Brian Rogers <bobbymcr@gmail.com>
2019-12-22 11:10:56 -0800

    Add benchmark project

---
+++ TDigest.Benchmarks/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+
+namespace StatsLib.Benchmarks {
+    class Program {
+        static void Main() {
+            BenchmarkRunner.Run<SortedDigest>();
+        }
+    }
+
+    [SimpleJob(RuntimeMoniker.Net461)]
+    [MemoryDiagnoser]
+    public class SortedDigest {
+        [Params(1_000, 10_000, 100_000)]
+        public int N { get; set; }
+
+        [Params(true, false)]
+        public bool Asc { get; set; }
+
+        [Benchmark]
+        public double Build() {
+            TDigest digest = new TDigest();
+            double denominator = N;
+            for (int i = 0; i < N; ++i) {
+                // Add normalized values in interval [0.0, 1.0)
+                int numerator = Asc ? i : (N - i - 1);
+                digest.Add(numerator / denominator);
+            }
+
+            double count = digest.Count;
+            if (count != N) {
+                throw new InvalidOperationException($"Bad benchmark; digest.Count ({count}) != N ({N})");
+            }
+
+            return count;
+        }
+    }
+}
